aa4692703edcfb32ced8269fbbbd57bcb5c07ef2
dbJaz <jazdibuccio@gmail.com>
2025-03-24 20:08:35 -0300

    graf 2 sin linea

--- codigo/graf2.py
+++ codigo/graf2.py
@@ -5,19 +5,24 @@ import armoArrays as ar
 import standardDeviation as sd
 
 array=ar.data
-peso=array[3]
-peso=np.delete(peso, -1)
+masa=array[3]
 inc_peso=array[3][-1]
-inc_diametro=array[7][-1]
-diametro=array[7]
-diametro=np.delete(diametro, -1)
-#print("el peso es", peso)
-plt.figure(figsize=(4, 3))
-plt.errorbar(peso, diametro, xerr=inc_peso, yerr=inc_diametro, fmt='o',ms=3,ls='none',capsize=3,zorder=0)
+masa=np.delete(masa, -1)
 
-plt.xscale('log')
-plt.yscale('log')
+diametro=array[7]
+inc_diametro=array[7][-1]
+print(inc_diametro)
+inc_dc = inc_diametro * 2 *diametro
+diametro=np.delete(diametro, -1)
+print("VALOR")
+print(diametro)
+print(inc_diametro)
+
+
+plt.figure(figsize=(4, 3))
+plt.errorbar(masa**2, diametro, xerr=inc_peso, yerr=inc_diametro, fmt='o',ms=3,ls='none',capsize=3,zorder=0)
 
 plt.xlabel('Peso [g]')
 plt.ylabel('Diámetro [cm]')
 plt.show()
+
