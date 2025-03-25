51a450ad642faa07368a6464a17d762b5f037d6c
dbJaz <jazdibuccio@gmail.com>
2025-03-24 23:25:40 -0300

    Graf 1 y 3 oficio listos

--- Oficio/Ograf3.py
+++ Oficio/Ograf3.py
@@ -4,3 +4,27 @@ import pandas as pd
 import OarmoArrays as ar
 import OstandardDeviation as sd
 from scipy import stats
+
+masa = ar.data[9]
+inc_masa = ar.data[9][-1]
+masa=np.delete(masa, -1)
+
+
+area = ar.data[2]
+inc_area = ar.data[2][-1]
+area = np.delete(area, -1)
+
+ajuste = stats.linregress(area, masa)
+
+plt.figure(figsize=(5, 4))
+plt.errorbar(area,masa,
+             xerr=inc_area, yerr=inc_masa,
+             fmt='o', ms=4, ls='none', capsize=3, zorder=0)
+
+y_pred = ajuste.slope * area + ajuste.intercept # Predicción de recta
+plt.plot(area, y_pred,c='r')
+
+plt.xlabel("Área [cm^2]")
+plt.ylabel("Masa [g]")
+plt.title("Masa en funcion del área, hoja Oficio")
+plt.show()
