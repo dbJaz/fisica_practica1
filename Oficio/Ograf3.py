cc69c60ec3478d8bab1891d30381d2e25e54c6df
dbJaz <jazdibuccio@gmail.com>
2025-03-31 00:03:55 -0300

    linea de regresion graf 3

--- Oficio/Ograf3.py
+++ Oficio/Ograf3.py
@@ -10,24 +10,20 @@ inc_masa = 0.1#la incertidumbre instrumental
 
 area = ar.data[2]
 inc_area = []
-for i in range(9):
+for i in range(8):
     area1=ar.data[2][i]
     lado1=ar.data[0][i]
     lado2=ar.data[1][i]
     incerteza = area1*((0.5/lado1)+(0.5/lado2))
     inc_area = np.append(inc_area,incerteza)
 
-print(inc_area)
-print(type(area), type(masa))
-print(area, masa)
-
-ajuste = stats.linregress(area, masa)
 
 plt.figure(figsize=(5, 4))
 plt.errorbar(area,masa,
              xerr=inc_area, yerr=inc_masa,
              fmt='o', ms=4, ls='none', capsize=3, zorder=0)
 
+ajuste = stats.linregress(area, masa)
 y_pred = ajuste.slope * area + ajuste.intercept # Predicción de recta
 plt.plot(area, y_pred,c='r')
 
